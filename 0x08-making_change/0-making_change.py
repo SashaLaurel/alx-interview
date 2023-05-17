9bc0439ade86c906d3ac3f01ae2419dd700cb177
SashaLaurel <enquiries@sashalaurel.net>
2023-05-17 14:01:03 -0700

    interview preparation

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+   Determines the fewest number of coins needed to meet
+   a given amount total.
+'''
+import sys
+
+
+def makeChange(coins, total):
+    '''
+    Return: 
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you 
+    have, return -1
+    '''
+    sum = 0
+    if (total <= 0):
+        return 0
+    coins.sort(reverse=True)
+    for i in coins:
+        if (total < i):
+            pass
+        q, r = divmod(total, i)
+        total = r
+        sum += q
+    if (total != 0):
+        return -1
+    return sum
